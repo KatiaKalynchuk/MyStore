2e6d64695a70f025e9d3fbe30fe3cc3d02c4f7f8
katia <katerynakalynchuk@gmail.com>
2017-11-22 17:26:45 +0200

    Added page for one phone

--- src/selectors.js
+++ src/selectors.js
@@ -1,6 +1,6 @@
 import R from 'ramda'
 
-const getPhonesById = (state, id) => R.prop(id, state.phones);
+export const getPhonesById = (state, id) => R.prop(id, state.phones);
 
 export const getPhones = state => {
     const phones = R.map(id => getPhonesById(state, id), state.phonesPage.ids);
